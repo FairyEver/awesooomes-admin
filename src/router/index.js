08ec8cd9ed8ca483664fdea2999a51fa9fa0941a
liyang <1711467488@qq.com>
2018-05-24 16:16:14 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import path from 'path'
 
 Vue.use(Router)
+
+const req = require.context('@/page/menuHide', false, /\.vue$/)
+const demos = req.keys().map(req).map(e => {
+  const def = e.default
+  const name = path.basename(def.__file, '.vue')
+  def.routerPath = name
+  def.routerName = name
+  return def
+})
+
+console.log(demos)
 
 export default new Router({
   routes: [
