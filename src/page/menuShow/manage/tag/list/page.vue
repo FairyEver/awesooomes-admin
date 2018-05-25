0eb4134e6e612a6eb01457e37ea8b05a7845d297
liyang <1711467488@qq.com>
2018-05-25 14:45:35 +0800

    no message

--- src/page/menuShow/manage/tag/list/page.vue
+++ src/page/menuShow/manage/tag/list/page.vue
@@ -1,26 +1,52 @@
 <template>
   <el-container class="page">
-    <el-table v-bind="table">
-      <el-table-column prop="id" label="ID" width="100"></el-table-column>
-      <el-table-column prop="name" label="名称"></el-table-column>
-      <el-table-column label="操作" width="300">
-        <template slot-scope="scope">
-          <el-button
-            size="small"
-            plain
-            @click="handleEdit(scope)">
-            修改
-          </el-button>
-          <el-button
-            size="small"
-            type="danger"
-            plain
-            @click="handleDelete(scope)">
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-card>
+      <div slot="header" class="clearfix">
+        <h1 class="mt-0 mb-0">Tag 标签管理</h1>
+        <p class="mt-5">管理你的标签</p>
+        <div>
+          <el-form :inline="true">
+            <el-form-item>
+              <el-button type="primary" @click="handleNew">新建</el-button>
+            </el-form-item>
+            <el-form-item>
+              <el-input placeholder="tag name"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="handleSearch">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+      <el-table v-bind="table">
+        <el-table-column prop="id" label="ID" width="100"></el-table-column>
+        <el-table-column prop="name" label="名称"></el-table-column>
+        <el-table-column label="操作" width="300">
+          <template slot-scope="scope">
+            <el-button
+              size="small"
+              plain
+              @click="handleEdit(scope)">
+              修改
+            </el-button>
+            <el-button
+              size="small"
+              type="danger"
+              plain
+              @click="handleDelete(scope)">
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="100">
+      </el-pagination>
+    </el-card>
   </el-container>
 </template>
 
@@ -55,6 +81,15 @@ export default {
         })
     },
     /**
+     * 删除一个tag
+     */
+    deleteTag (id) {
+      this.$http.delete(`tag/${id}`)
+        .then(res => {
+          this.getData()
+        })
+    },
+    /**
      * 接收编辑事件
      */
     handleEdit (scope) {
@@ -64,7 +99,19 @@ export default {
      * 接收删除事件
      */
     handleDelete (scope) {
-      console.log(scope)
+      this.deleteTag(scope.row.id)
+    },
+    /**
+     * 接收新增事件
+     */
+    handleNew () {
+      //
+    },
+    /**
+     * 接收搜索事件
+     */
+    handleSearch () {
+      //
     }
   }
 }
@@ -72,5 +119,9 @@ export default {
 
 <style lang="scss" scoped>
 .page {
+  flex-direction: column;
+  .el-form-item {
+    margin-bottom: 0px;
+  }
 }
 </style>
